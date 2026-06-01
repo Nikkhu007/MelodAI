ae964f39bbf7585b4a6a9a600d1b775ee41a516b
Nikkhu <njborse007@gmail.com>
2026-06-01 22:09:38 +0530

    Allow X-Client-Version header

--- backend/server.js
+++ backend/server.js
@@ -100,7 +100,12 @@ app.use(cors({
   },
   credentials:     true,
   methods:         ['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
-  allowedHeaders:  ['Content-Type','Authorization','X-Request-Id'],
+  allowedHeaders: [
+  'Content-Type',
+  'Authorization',
+  'X-Request-Id',
+  'X-Client-Version'
+],
   exposedHeaders:  ['X-Request-Id','X-Cache','X-RateLimit-Remaining'],
 }))
 app.options('*', cors())
